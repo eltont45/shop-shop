fb736cb132c26b03e2d8fd5b40b18a915581fcdb
eltont45 <83630909+eltont45@users.noreply.github.com>
2022-04-19 19:10:59 -0700

    Stripe

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,14 +1,13 @@
-import React, { useEffect} from 'react';
-import { useQuery } from '@apollo/client';
-
+import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import spinner from '../../assets/spinner.gif';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { idbPromise } from "../../utils/helpers";
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCTS } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+import spinner from '../../assets/spinner.gif';
 
-function ProductList({ currentCategory }) {
+function ProductList() {
   const [state, dispatch] = useStoreContext();
 
   const { currentCategory } = state;
@@ -16,25 +15,19 @@ function ProductList({ currentCategory }) {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
-    // if there's data to be stored
     if (data) {
-      // let's store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products
+        products: data.products,
       });
-
-      // but let's also take each product and save it to the IndexedDB using the helper function
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
       });
     } else if (!loading) {
-      // since we're offline, get all of the data from the `products` store
       idbPromise('products', 'get').then((products) => {
-        // use retrieved data to set global state for offline browsing
         dispatch({
           type: UPDATE_PRODUCTS,
-          products: products
+          products: products,
         });
       });
     }
@@ -45,13 +38,15 @@ function ProductList({ currentCategory }) {
       return state.products;
     }
 
-    return state.products.filter(product => product.category._id === currentCategory);
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {products.length ? (
+      {state.products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
